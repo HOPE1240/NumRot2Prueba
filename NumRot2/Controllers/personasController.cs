e04da2440ba65229ad6274db92d6260ba84108d4
santiago <uribesantiago12@gmail.com>
2024-09-18 02:33:11 -0500

    Agregar archivos de proyecto.

---
+++ NumRot2/Controllers/personasController.cs
@@ -0,0 +1,146 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using NumRot2.Models;
+
+namespace NumRot2.Controllers
+{
+    [Authorize]
+    public class personasController : Controller
+    {
+        private NumEntities db = new NumEntities();
+
+        // GET: personas
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // GET: personas/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            var persona = db.personas.Find(id);
+            if (persona == null)
+            {
+                return HttpNotFound();
+            }
+            return View(persona);
+        }
+
+        // GET: personas/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: personas/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "id,numero_cedula,primer_nombre,segundo_nombre,primer_apellido,segundo_apellido,telefono,correo,direccion,edad,genero")] personas persona)
+        {
+            if (ModelState.IsValid)
+            {
+                db.personas.Add(persona);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(persona);
+        }
+
+        // GET: personas/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            var persona = db.personas.Find(id);
+            if (persona == null)
+            {
+                return HttpNotFound();
+            }
+            return View(persona);
+        }
+
+        // POST: personas/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "id,numero_cedula,primer_nombre,segundo_nombre,primer_apellido,segundo_apellido,telefono,correo,direccion,edad,genero")] personas persona)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(persona).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(persona);
+        }
+
+        // GET: personas/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            var persona = db.personas.Find(id);
+            if (persona == null)
+            {
+                return HttpNotFound();
+            }
+            return View(persona);
+        }
+
+        // POST: personas/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            var persona = db.personas.Find(id);
+            db.personas.Remove(persona);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        // GET: personas/GetDataPersonas
+        public JsonResult GetDataPersonas()
+        {
+            var personas = db.personas.Select(p => new
+            {
+                p.id,
+                p.numero_cedula,
+                p.primer_nombre,
+                p.segundo_nombre,
+                p.primer_apellido,
+                p.segundo_apellido,
+                p.telefono,
+                p.correo,
+                p.direccion,
+                p.edad,
+                p.genero
+            }).ToList();
+
+            return Json(new { data = personas }, JsonRequestBehavior.AllowGet);
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+
+}
---
+++ NumRot2/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="NumRot2.MvcApplication" Language="C#" %>
